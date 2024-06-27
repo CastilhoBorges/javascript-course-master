a56a667d5ff2933bde7bed46f10660bcf7dd467d
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-06-27 10:59:41 -0300

    change and challenge 02

--- 02-Fundamentals-Part-2/challenge02.js
+++ 02-Fundamentals-Part-2/challenge02.js
@@ -1,6 +1,6 @@
 
 const calcTip = function(bill) {
-    return bill >= 50 && bill <= 300 ? bill * (15 / 100) : bill * (20 / 100);
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
 };
 console.log(calcTip(100));
 
@@ -9,4 +9,7 @@ const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
 console.log(tips);
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
 
