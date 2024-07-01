c1ab900761a14840fb3e4dbeaf4c6a895f32f4b6
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-07-01 16:07:28 -0300

    Debuggin

---
+++ 03-Developer-Skills/debuggin.js
@@ -0,0 +1,17 @@
+/*const kelvin = function() {
+    const measuRement = {
+        typeo: 'temp',
+        unit: 'celsius',
+        // C) FIX BUG
+        value: Number(prompt('Degrees celsius:'))
+    };
+
+    // B) FIND BUG
+    console.table(measuRement);
+
+    const kelvin = measuRement.value + 273;
+    return kelvin;
+}
+// A) IDENTIFY 
+console.log(kelvin());*/
+
