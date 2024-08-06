6c5da6252d07a25e893c388782e061ee7d2e734e
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-08-06 15:52:28 -0300

    padEnd, padStart, repeat

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -47,7 +47,7 @@ console.log(p, q, r);
 */
 
 //////////////////////////////////////////////////
-const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+/*const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 const openingHours = {
   [weekdays[3]]: {
     open: 12,
@@ -88,7 +88,7 @@ const restaurant = {
     );
   },
 };
-
+*/
 /*restaurant.orderDelivery(0, 2, );
 
 const { name, openingHours, categories } = restaurant;
@@ -300,7 +300,7 @@ console.log(myName.slice(1, 4));
 
 console.log(myName.toLowerCase()); // Minusculo
 console.log(myName.toUpperCase()); // Maisculo
-*/
+
 // Exemplo pratico toLower and toUpper
 const passager = "aUgUstO";
 // Queremos que este nome fique com a primeira maiuscula e o resto minuscula
@@ -355,3 +355,93 @@ const checkBaggage = function (items) {
 
 checkBaggage("I have a notebook, a Key, a Shirt and a phone");
 checkBaggage("Snaks, hat");
+
+
+// Split e Join
+console.log("i,love,you,baby".split(","));
+// console.log("Augusto Castilho");
+
+const [firstName, lastName] = "Augusto Castilho".split(" ");
+console.log(firstName, lastName);
+
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(" ");
+  const upperNames = [];
+
+  for (const n of names) {
+    upperNames.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+
+  console.log(upperNames.join(" "));
+};
+
+capitalizeName("augusto castilho borges");
+
+// Padding
+let message = "Go to gate 23!";
+console.log(message.length);
+message = message.padStart(40, "+");
+console.log(message.length, message);
+
+const maskCreditCard = function (number) {
+  const str = number + "";
+  const lastNumbers = str.slice(-4);
+  console.log(lastNumbers.padStart(str.length, "*"));
+};
+
+maskCreditCard(5502095495128243);
+
+// Repeat
+const message2 = "O tempo esta ruim... Todos os portões fechados";
+console.log(message2.repeat(1));
+///////////////////////////////////////////////////////////
+
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", () => {
+  const infos = document.querySelector("textarea").value;
+  const arrInfos = infos.split("\n");
+  const arrNew = [];
+
+  for (const [index, i] of arrInfos.entries()) {
+    const [first, second] = i.trim().toLowerCase().split("_");
+    const outPut = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+
+    console.log(`${outPut.padEnd(20)}${"✅".repeat(index + 1)}`);
+  }
+});
