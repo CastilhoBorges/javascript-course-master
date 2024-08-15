4475e06c149c88fec665e8240c6d18231f629161
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-08-15 12:04:00 -0300

    Bind e inicio de desafio

--- 10-Functions/challenges.js
+++ 10-Functions/challenges.js
@@ -16,7 +16,7 @@ const forTwo = function (element) {
 
 const transformar = (array, fn) => console.log(fn(array));
 
-transformar([2, 2, 2], forTwo);
+//transformar([2, 2, 2], forTwo);
 
 /*
 2. Função de Filtro
@@ -35,7 +35,7 @@ const IfEven = function (array) {
 
 const filtrar = (array, fn) => console.log(fn(array));
 
-filtrar([1, 2, 3, 4, 5, 6, 7, 8, 9], IfEven);
+//filtrar([1, 2, 3, 4, 5, 6, 7, 8, 9], IfEven);
 
 /*
 3. Funções como Argumentos
@@ -49,4 +49,56 @@ Crie uma função aplicar(lista, funcao) que recebe uma lista e uma função, e 
 const soma = (a, b) => a + b;
 
 const operar = (a, b, opFn) => console.log(opFn(a, b));
-operar(2, 3, soma);
+//operar(2, 3, soma);
+
+// Exercícios com call
+
+/*
+Função de saudação:
+Crie uma função saudacao que recebe dois parâmetros: cumprimento e nome. Use o método call para invocar essa função com diferentes objetos que tenham a propriedade nome.
+*/
+
+function saudacao(cumprimento, nome) {
+  console.log(cumprimento + ", " + nome + "!");
+}
+
+const pessoa1 = { nome: "Carlos" };
+const pessoa2 = { nome: "Maria" };
+
+saudacao.call(pessoa1, "Ola", pessoa1.nome);
+saudacao.call(pessoa2, "Ola", pessoa2.nome);
+
+/*
+Função matemática:
+Crie uma função que calcule a área de um retângulo e use call para calcular a área de diferentes objetos que representem retângulos com diferentes propriedades largura e altura.
+*/
+
+function calculArea() {
+  console.log(this.largura * this.altura);
+}
+
+const retangulo1 = { largura: 5, altura: 10 };
+const retangulo2 = { largura: 7, altura: 3 };
+
+calculArea.call(retangulo1);
+calculArea.call(retangulo2);
+
+/*
+Herança com call:
+Crie uma função construtora Animal que recebe nome e som. Em seguida, crie uma função construtora Cachorro que herda de Animal usando call.
+*/
+const Animal = function (nome, som) {
+  this.nome = nome;
+  this.som = som;
+  this.fazerSom = function () {
+    console.log(`${this.nome} faz ${this.som}`);
+  };
+};
+
+const Cachorro = function (nome) {
+  Animal.call(this, nome, "latido");
+};
+
+const dog = new Cachorro("Toddy");
+dog.fazerSom();
+
