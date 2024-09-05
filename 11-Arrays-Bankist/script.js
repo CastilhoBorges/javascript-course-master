8a3a43eee1e37cc4e10c3753c9c2dcc8a7e62996
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-09-05 17:07:05 -0300

    Método Find

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -109,7 +109,6 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts);
-console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -310,7 +309,7 @@ TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
 TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
-*/
+
 const calcAverageHumanAge = (arr) =>
   arr
     .map((age) => (age <= 2 ? age * 2 : age * 4 + 16))
@@ -319,3 +318,16 @@ const calcAverageHumanAge = (arr) =>
 
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+const mov = movements.find(mov => mov < 0);
+console.log(movements, mov);
+*/
+
+for (const i of accounts) {
+  if (i.owner === "Jessica Davis") {
+    console.log(i);
+  }
+}
+
+const davis = accounts.find((acc) => acc.owner === "Jessica Davis");
+console.log(davis);
