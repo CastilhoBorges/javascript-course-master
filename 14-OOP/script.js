7ee9b8cfb1e60c57ec0f75a50d4253cb1b4bf173
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-10-01 14:54:41 -0300

    JSON

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -307,7 +307,7 @@ ford.speedUS;
 console.log(ford);
 ford.speedUS = 10;
 console.log(ford);
-*/
+
 
 class Person {
   name;
@@ -399,3 +399,55 @@ class Quadrado extends Shape {
 const q1 = new Quadrado(10);
 q1.calcArea();
 q1.calcPerimetro();
+
+
+const superHero = {
+  squadName: "Super hero squad",
+  homeTown: "Metro City",
+  formed: 2016,
+  secretBase: "Super tower",
+  active: true,
+  members: [
+    {
+      name: "Molecule Man",
+      age: 29,
+      secretIdentity: "Dan Jukes",
+      powers: ["Radiation resistance", "Turning tiny", "Radiation blast"],
+    },
+    {
+      name: "Madame Uppercut",
+      age: 39,
+      secretIdentity: "Jane Wilson",
+      powers: [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes",
+      ],
+    },
+    {
+      name: "Eternal Flame",
+      age: 1000000,
+      secretIdentity: "Unknown",
+      powers: [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel",
+      ],
+    },
+  ],
+};
+
+console.log(superHero.members[1].powers[1]);
+*/
+
+const alo = {
+  nome: "Augusto",
+};
+
+console.log(typeof alo);
+console.log(JSON.stringify(alo));
+
+const jsonAlo = JSON.stringify(alo);
+console.log(JSON.parse(jsonAlo));
