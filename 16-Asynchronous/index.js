0feb2a6419a7b2926fa33c841e840c3dc590e5c4
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-10-03 15:02:35 -0300

    Promises

--- 16-Asynchronous/index.js
+++ 16-Asynchronous/index.js
@@ -28,14 +28,66 @@ promise
   });
 
 numero.innerHTML = "Processando...";
+
+
+console.log("Augusto");
+setTimeout(() => console.log("Castilho"), 500);
+console.log("Borges");
+
+/// Promises ///
+
+const fetchPromises = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+);
+
+console.log(fetchPromises);
+
+fetchPromises
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+
+    return response.json();
+  })
+  .then((data) => console.log(data[5].name))
+  .catch((error) => {
+    console.error(`Não foi possivel obter os produtos: ${error}`);
+  });
+
+console.log("Iniciado");
+
+
+const fetchPromise1 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+);
+const fetchPromise2 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
+);
+const fetchPromise3 = fetch(
+  "bad-scheme://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+);
+
+Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+  .then((responses) => {
+    for (const response of responses) {
+      console.log(`${response.url}: ${response.status}`);
+    }
+  })
+  .catch((error) => {
+    console.error(`Falha ao buscar: ${error}`);
+  });
+
+
+const myPromise = new Promise((res, rej) => {
+  let sucesso = false;
+
+  if (sucesso) {
+    res(`${sucesso} DEU BOM`);
+  } else {
+    rej("DEU RUIM");
+  }
+});
+
+myPromise.then((res) => console.log(res)).catch((err) => console.log(err));
 */
-
-const asyncAwait = async function () {
-  const nome = "Augusto";
-
-  await console.log(nome);
-};
-
-console.log();
-console.log();
-console.log();
